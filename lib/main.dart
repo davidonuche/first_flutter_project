793bf1e045e2451e3b2f53bdc967eb56de5e8928
David onuche <110064207+Davidbeebest@users.noreply.github.com>
2023-03-04 22:22:54 +0100

    done with first flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// This application is my first appliction and it is a simple appliction
+// It generate an Image from flutter.dev/doc/cookbook/display images from the internet, on the screen.
+
 import 'package:first_flutter_project/screens/my_app_screen.dart';
 import 'package:flutter/material.dart';
 
